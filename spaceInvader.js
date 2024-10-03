458416d571290479bc211032a046cbadf5db32c5
Nathan Mingole <nathan.mingole@student.ehb.be>
2024-10-03 15:21:21 +0200

    Space invader klaar

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,45 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+spaceinvader();
+
+function spaceinvader() {
+	context.beginPath();
+	context.fillStyle = "black";
+	context.rect(50, 50, 300, 300);
+	context.fill();
+
+	context.beginPath();
+	context.fillStyle = "#83f52c";
+	context.rect(75, 75, 50, 50);
+	context.fill();
+
+	context.beginPath();
+	context.fillStyle = "#83f52c";
+	context.rect(175, 75, 50, 50);
+	context.fill();
+
+	context.beginPath();
+	context.fillStyle = "#83f52c";
+	context.rect(275, 75, 50, 50);
+	context.fill();
+
+	context.beginPath();
+	context.fillStyle = "#83f52c";
+	context.rect(125, 275, 50, 50);
+	context.fill();
+
+	context.beginPath();
+	context.fillStyle = "#83f52c";
+	context.rect(175, 225, 50, 50);
+	context.fill();
+
+	context.beginPath();
+	context.fillStyle = "#83f52c";
+	context.rect(22 5, 275, 50, 50);
+	context.fill();
+}
